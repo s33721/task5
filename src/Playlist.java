6c1b870879e93d40145144ebf7792bbec6bc7406
s33721 <s33721@pjwstk.edu.pl>
2025-04-04 16:07:28 +0200

    playlist class got created

---
+++ src/Playlist.java
@@ -0,0 +1,45 @@
+import javax.sound.midi.Track;
+import java.util.ArrayList;
+
+public class Playlist {
+    private ArrayList<String> tracks;
+    private String name;
+
+    public Playlist(String name) {
+        this.name = name;
+        tracks = new ArrayList<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void addTrack(String track) {
+        if (!tracks.contains(track)) {
+            this.tracks.add(track);
+        } else {
+            System.out.println("Track " + track + " already exists");
+        }
+    }
+
+    public void removeTrack(String track) {
+        boolean isRemoved = this.tracks.remove(track);
+        if (!isRemoved) {
+            System.out.println("Track " + track + " does not exist");
+        }
+        tracks.remove(track);
+    }
+    public void display() {
+        System.out.println("Tracks: " + this.name);
+        if (this.tracks.isEmpty()) {
+            System.out.println("Nothing to show");
+        } else {
+            for (String track : this.tracks) {
+                System.out.println("-" + track);
+            }
+        }
+    }
+
+
+
+}
